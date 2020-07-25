230ee9927f5cd2c84810766c2d2f4ba1ad91f47b
Valentyn Tiron <vt02@Valentyns-MacBook-Pro.local>
2020-07-25 00:38:32 -0400

    added new project

--- src/projects.js
+++ src/projects.js
@@ -1,54 +1,60 @@
 const projects = [
   {
-    name: "MERNAP",
-    img: require("./images/mernap.png"),
-    info:
-      "Made with MERN Stack. Has login/register and user search with posts. Also deployed with heroku.",
-    codeLink: "https://github.com/vtiron2002/mernap",
-    pageSite: "http://mernap.herokuapp.com/",
+    name: 'Express-Sell',
+    img: require('./images/express-sell.png'),
+    info: 'MERN stack app which includes users, orders, and products in the mongodb. Also deployed on heroku',
+    codeLink: 'https://github.com/vtiron2002/express-sell',
+    pageSite: 'https://express-sell.herokuapp.com/'
   },
   {
-    name: "BookRooms",
-    img: require("./images/bookRooms.png"),
-    info: "Has map integrated with React with a nice deisgn.",
-    codeLink: "https://github.com/vtiron2002/book-rooms",
-    pageSite: "https://vtiron2002.github.io/book-rooms/",
+    name: 'MERNAP',
+    img: require('./images/mernap.png'),
+    info: 'Made with MERN Stack. Has login/register and user search with posts. Also deployed with heroku.',
+    codeLink: 'https://github.com/vtiron2002/mernap',
+    pageSite: 'http://mernap.herokuapp.com/'
   },
   {
-    name: "LoginPage",
-    img: require("./images/loginPage.png"),
-    info: "Simple design mockup for a login page.",
-    codeLink: "https://github.com/vtiron2002/login-page",
-    pageSite: "https://vtiron2002.github.io/login-page/",
+    name: 'BookRooms',
+    img: require('./images/bookRooms.png'),
+    info: 'Has map integrated with React with a nice deisgn.',
+    codeLink: 'https://github.com/vtiron2002/book-rooms',
+    pageSite: 'https://vtiron2002.github.io/book-rooms/'
   },
   {
-    name: "DarkHelpPage",
-    img: require("./images/dark-help-page.png"),
-    info: "Simple design mockup of a dark help page.",
-    codeLink: "https://github.com/vtiron2002/dark-help-page",
-    pageSite: "https://vtiron2002.github.io/dark-help-page/",
+    name: 'LoginPage',
+    img: require('./images/loginPage.png'),
+    info: 'Simple design mockup for a login page.',
+    codeLink: 'https://github.com/vtiron2002/login-page',
+    pageSite: 'https://vtiron2002.github.io/login-page/'
   },
   {
-    name: "FastTechSite",
-    img: require("./images/fastTechSite.png"),
-    info: "CRUD React app with creating, editing and deleting items.",
-    codeLink: "https://github.com/vtiron2002/fast-tech-site-BUILD/tree/master",
-    pageSite: "https://vtiron2002.github.io/fast-tech-site-BUILD/",
+    name: 'DarkHelpPage',
+    img: require('./images/dark-help-page.png'),
+    info: 'Simple design mockup of a dark help page.',
+    codeLink: 'https://github.com/vtiron2002/dark-help-page',
+    pageSite: 'https://vtiron2002.github.io/dark-help-page/'
   },
   {
-    name: "MobileUI",
-    img: require("./images/mobileUI.png"),
-    info: "Design mockup for a mobile app.",
-    codeLink: "https://github.com/vtiron2002/MockupInterviewCodeTest",
-    pageSite: "https://vtiron2002.github.io/MockupInterviewCodeTest/",
+    name: 'FastTechSite',
+    img: require('./images/fastTechSite.png'),
+    info: 'CRUD React app with creating, editing and deleting items.',
+    codeLink: 'https://github.com/vtiron2002/fast-tech-site-BUILD/tree/master',
+    pageSite: 'https://vtiron2002.github.io/fast-tech-site-BUILD/'
   },
   {
-    name: "NoteMan",
-    img: require("./images/noteman.png"),
-    info: "Design mockup with responiveness.",
-    codeLink: "https://github.com/vtiron2002/noteman",
-    pageSite: "https://vtiron2002.github.io/noteman/",
+    name: 'MobileUI',
+    img: require('./images/mobileUI.png'),
+    info: 'Design mockup for a mobile app.',
+    codeLink: 'https://github.com/vtiron2002/MockupInterviewCodeTest',
+    pageSite: 'https://vtiron2002.github.io/MockupInterviewCodeTest/'
   },
-];
+  {
+    name: 'NoteMan',
+    img: require('./images/noteman.png'),
+    info: 'Design mockup with responiveness.',
+    codeLink: 'https://github.com/vtiron2002/noteman',
+    pageSite: 'https://vtiron2002.github.io/noteman/'
+  }
+]
 
-export default projects;
+export default projects
